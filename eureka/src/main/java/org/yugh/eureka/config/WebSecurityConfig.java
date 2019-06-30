22c513c5de736309d1cde1819e4f468ed6ef3d89
Genhai Yu <genhaiyu@genhai.io>
2019-06-30 20:41:46 +0800

    modify eureka and security

---
+++ eureka/src/main/java/org/yugh/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package org.yugh.eureka.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+/**
+ * //
+ * 1: 使用Spring Security是比较方便的，不需要重写很多自定义的拦截代码
+ * 2: MVC的 WebMvcConfigurationSupport 或者 HandlerInterceptor 自定义拦截器需要手动重写，用在springboot架构上不合适
+ *
+ * @author: 余根海
+ * @creation: 2019-06-30 19:14
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final AccessDeniedHandler accessDeniedHandler;
+    private final EurekaAuthenticationEntryPoint eurekaAuthenticationEntryPoint;
+
+    @Autowired
+    public WebSecurityConfig(@Qualifier("eurekaAuthenticationAccessDeniedHandler") AccessDeniedHandler accessDeniedHandler,
+                             EurekaAuthenticationEntryPoint eurekaAuthenticationEntryPoint) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.eurekaAuthenticationEntryPoint = eurekaAuthenticationEntryPoint;
+    }
+
+
+    /**
+     * 非http://localhost:8010/和健康检查http://localhost:8010/actuator，其他统一不允许
+     *
+     *
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler).and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(eurekaAuthenticationEntryPoint).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers("/", "/actuator/**")
+                .permitAll()
+                .anyRequest().authenticated();
+        http.headers().cacheControl();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web
+                .ignoring()
+                .antMatchers(
+                        "/favicon.ico",
+                        "/eureka/**"
+                );
+    }
+
+}

88fbff9291d3a640b386184732ac906a42f44af0
Genhai Yu <genhaiyu@genhai.io>
2019-07-01 17:01:55 +0800

    add monitor、product

---
+++ product/src/main/java/org/yugh/product/controller/IndexController.java
@@ -0,0 +1,31 @@
+package org.yugh.product.controller;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * //
+ *
+ * @author: 余根海
+ * @creation: 2019-07-01 16:27
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@Slf4j
+@RestController
+@Api(description = "index控制器")
+public class IndexController {
+
+
+    @GetMapping("index1")
+    public Object index1(HttpServletRequest request){
+
+        log.info("========> url:{}", request.getServletPath());
+
+        return request.getServletPath();
+    }
+
+}

b1ab83249ffc43bb875d50f6d7fb1012f97ab7ea
yugenhai <yugenhai@yingding.org>
2019-07-01 21:01:54 +0800

    add hystrix dashboard

---
+++ product/src/main/java/org/yugh/product/service/IndexService.java
@@ -0,0 +1,27 @@
+package org.yugh.product.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.yugh.product.fallback.IndexFallbackFactory;
+
+/**
+ * //
+ *
+ * @author: 余根海
+ * @creation: 2019-07-01 19:10
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@FeignClient(name = "customer", fallbackFactory = IndexFallbackFactory.class)
+public interface IndexService {
+
+
+    /**
+     * 调用消费者
+     * @param randomId
+     * @return
+     */
+    @GetMapping("getRandomId/{randomId}")
+    Object getRandomId(@PathVariable("randomId") Long randomId);
+
+}
---
+++ product/src/main/java/org/yugh/product/service/impl/IndexTimeOutFallbackImpl.java
@@ -0,0 +1,23 @@
+package org.yugh.product.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.yugh.product.service.IndexService;
+
+/**
+ * //超时熔断
+ *
+ * @author: 余根海
+ * @creation: 2019-07-01 19:14
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@Slf4j
+@Component("indexTimeOutFallbackImpl")
+public class IndexTimeOutFallbackImpl implements IndexService {
+
+    @Override
+    public Object getRandomId(Long randomId) {
+        log.info("========> 超时熔断:{}", randomId);
+        return "超时熔断";
+    }
+}

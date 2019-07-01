067540385be07d5c85bf5af080b57bed6bcde03e
Genhai <genhai@genhai.dev>
2019-07-01 21:01:54 +0800

    add hystrix dashboard

---
+++ customer/src/main/java/org/yugh/customer/controller/IndexController.java
@@ -0,0 +1,46 @@
+package org.yugh.customer.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.yugh.customer.utils.SnowFlakeUtil;
+
+/**
+ * //
+ *
+ * @author: 余根海
+ * @creation: 2019-07-01 19:03
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@Slf4j
+@RestController
+@Api(description = "消费控制器")
+@DefaultProperties(defaultFallback = "defaultFallback")
+public class IndexController {
+
+
+    @GetMapping(value = "getRandomId/{randomId}")
+    public Object getCustomerById(@PathVariable("randomId") Long randomId) {
+
+
+        log.info("=====================>  收到来自productId: {} ", randomId);
+
+        if (randomId % 2 == 0) {
+            throw new RuntimeException("测试异常");
+
+        }
+
+        Object random = SnowFlakeUtil.nextWaterFlow();
+        return random;
+    }
+
+
+    private String defaultFallback() {
+
+        return "来自customer的提示： 太拥挤了， 请稍后再试。 ";
+    }
+
+}
---
+++ product/src/main/java/org/yugh/product/fallback/IndexFallbackFactory.java
@@ -0,0 +1,38 @@
+package org.yugh.product.fallback;
+
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.yugh.product.service.IndexService;
+
+import javax.annotation.Resource;
+
+/**
+ * //断路器工厂定制实现
+ *
+ * @author: 余根海
+ * @creation: 2019-06-24 20:23
+ * @Copyright © 2019 yugenhai. All rights reserved.
+ */
+@Slf4j
+@Component
+public class IndexFallbackFactory implements FallbackFactory<IndexService> {
+
+    private final static String TIME_OUT = "Read time out";
+
+    @Resource(name = "indexTimeOutFallbackImpl")
+    private IndexService indexTimeOutFallbackService;
+    @Resource(name = "indexErrorFallbackImpl")
+    private IndexService indexFallbackService;
+
+
+    @Override
+    public IndexService create(Throwable throwable) {
+        log.error("Feign getCustomerById 进入熔断工厂错误 = {}", throwable.getMessage());
+        if(null != throwable.getMessage() && throwable.getMessage().contains(TIME_OUT)){
+            return indexTimeOutFallbackService;
+        }else {
+            return indexFallbackService;
+        }
+    }
+}

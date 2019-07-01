cb2092834ca4eec24bf8447b280c0f7bf40575bd
Genhai Yu <genhaiyu@genhai.io>
2019-07-01 21:01:54 +0800

    add hystrix dashboard

--- monitor/src/main/java/org/yugh/monitor/MonitorApplication.java
+++ monitor/src/main/java/org/yugh/monitor/MonitorApplication.java
@@ -1,9 +1,7 @@
 package org.yugh.monitor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
@@ -14,10 +12,8 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * @creation: 2019-04-09 18:17
  * @Copyright © 2019 yugenhai. All rights reserved.
  */
-@EnableHystrix
 @EnableTurbine
-@EnableDiscoveryClient
-@SpringBootApplication
+@SpringCloudApplication
 @EnableHystrixDashboard
 //@EnableZipkinServer
 public class MonitorApplication {
--- product/src/main/java/org/yugh/product/controller/IndexController.java
+++ product/src/main/java/org/yugh/product/controller/IndexController.java
@@ -1,9 +1,16 @@
 package org.yugh.product.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.yugh.product.service.IndexService;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,15 +24,55 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RestController
 @Api(description = "index控制器")
+@DefaultProperties(defaultFallback = "defaultFallback")
 public class IndexController {
 
+    private final IndexService indexService;
+
+    @Autowired
+    public IndexController(IndexService indexService) {
+        this.indexService = indexService;
+    }
 
     @GetMapping("index1")
-    public Object index1(HttpServletRequest request){
-
+    public Object index1(HttpServletRequest request) {
         log.info("========> url:{}", request.getServletPath());
-
         return request.getServletPath();
+    }
+
+
+    /**
+     * HystrixCommand  会有依赖隔离， 隔离到一个线程以内， 不影响其他函数调用。
+     *
+     * @param randomId
+     * @return
+     */
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000"),
+            @HystrixProperty(name = "circuitBreaker.enabled", value = "true"), //开启熔断
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"), //最近10次服务
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60"),//最近10次服务，请求的成功率， 达到就熔断
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "2000"),// 休眠10秒后， 重新探测是否需要熔断。
+
+    })
+    @ApiOperation(value = "拿到一个随机数", notes = "测试")
+    @GetMapping("/index2/{randomId}")
+    public String info(@PathVariable("randomId") Long randomId) {
+        Object id = indexService.getRandomId(randomId);
+        log.info("========> 拿到来自customer的一个随机数:{}", id);
+        return "拿到来自customer的randomId:" + id;
+
+    }
+
+
+    private String fallback(Long productId) {
+
+        return "太拥挤了， 请稍后再试。";
+    }
+
+    private String defaultFallback() {
+
+        return "来自product的提示： 太拥挤了， 请稍后再试。 ";
     }
 
 }

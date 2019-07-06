16fbb51fa2cb2d8a6ae93d8c9a475a73827b5324
Genhai Yu <genhaiyu@gmail.com>
2019-07-06 12:29:05 +0800

    update zuulFilter

--- eureka/src/main/java/org/yugh/eureka/config/EurekaAuthenticationAccessDeniedHandler.java
+++ eureka/src/main/java/org/yugh/eureka/config/EurekaAuthenticationAccessDeniedHandler.java
@@ -1,17 +1,5 @@
 package org.yugh.eureka.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.stereotype.Component;
-import org.yugh.eureka.common.ResultJson;
-import org.yugh.eureka.enums.ResultEnum;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 /**
  * //无权限
  * 1:Authentication 在eureka里不用验证用户的信息
@@ -20,10 +8,10 @@ import java.io.PrintWriter;
  * @creation: 2019-06-30 19:14
  * @Copyright © 2019 yugenhai. All rights reserved.
  */
-@Slf4j
-@Component("eurekaAuthenticationAccessDeniedHandler")
-public class EurekaAuthenticationAccessDeniedHandler implements AccessDeniedHandler {
-
+//@Slf4j
+//@Component("eurekaAuthenticationAccessDeniedHandler")
+public class EurekaAuthenticationAccessDeniedHandler  {
+ /*implements AccessDeniedHandler
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException {
         response.setStatus(200);
@@ -34,5 +22,5 @@ public class EurekaAuthenticationAccessDeniedHandler implements AccessDeniedHand
         printWriter.flush();
         log.info("请求地址 :{}, 信息 : {}", request.getServletPath(),ResultEnum.FORBIDDEN.getValue());
 
-    }
+    }*/
 }

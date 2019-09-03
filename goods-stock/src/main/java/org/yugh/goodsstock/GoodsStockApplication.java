9aeb21536be7d92860f29d5d0112294515d1862a
Genhai <genhai@genhai.dev>
2019-09-03 15:24:12 +0800

    商品库存Demo移至framework-applications

---
+++ goods-stock/src/main/java/org/yugh/goodsstock/GoodsStockApplication.java
@@ -0,0 +1,20 @@
+package org.yugh.goodsstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 测试类在src/main/test/java/**
+ * <p>
+ * 单机秒杀商品库存
+ *
+ * @author yugenhai
+ */
+@SpringBootApplication
+public class GoodsStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsStockApplication.class, args);
+    }
+
+}

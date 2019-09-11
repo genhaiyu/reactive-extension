fb45f632e5f0ee1c0e5accd8846b9d3289f7cc3d
Genhai Yu <genhaiyu@genhai.io>
2019-09-11 17:53:33 +0800

    Fix Bug Goods Stock 商品秒杀时间存在并发线程安全，需要调整unix时间对比

--- goods-stock/src/main/java/org/yugh/goodsstock/GoodsStockApplication.java
+++ goods-stock/src/main/java/org/yugh/goodsstock/GoodsStockApplication.java
@@ -7,6 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 测试类在src/main/test/java/**
  * <p>
  * 单机秒杀商品库存
+ * <p>
+ * <p>
+ * FIXME 乐观锁的时间存在并发不安全，在设计上也不安全，需要修改为unix的时间版本来对比
+ * Modify 2019-09-11 {@link org.yugh.goodsstock.optimisticlock.repository.OptimisticLockRepository}
+ * <p>
+ * 若修改可参照 {@link `repository` InstantLongConverter} code {convertToDatabaseColumn}
+ * <p>
+ * Like this 例如 :
+ * <p>
+ * InstantLongConverter converter = new InstantLongConverter();
+ * System.out.println(converter.convertToDatabaseColumn(Instant.now()));
  *
  * @author yugenhai
  */
@@ -16,5 +27,4 @@ public class GoodsStockApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsStockApplication.class, args);
     }
-
 }

e829a4bb9a98957ea3ef9eb1206e76f33ba37085
Genhai <genhai@genhai.dev>
2019-10-18 16:07:46 +0800

    Rename

--- cqrs/src/main/java/org/yugh/cqrs/goods/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author yugenhai
- */
-package org.yugh.cqrs.goods;
-/**
- * Goods package
- * <p>
- * 商品测试的CQRS模型设计
- */
--- domain-event/src/main/java/org/yugh/domainevent/boot/daos/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.yugh.domainevent.boot.daos;
-
-import org.springframework.data.repository.CrudRepository;
-import org.yugh.domainevent.boot.domain.MerchandiseEntity;
-
-public interface InventoryRepository extends CrudRepository<MerchandiseEntity, Long> {
-}

463ea1bc3d74506435a9eca0fa76d81ab00c4d2c
Genhai Yu <genhaiyu@genhai.io>
2019-12-29 23:57:51 +0800

    :recycle: 删除一些多余文件
    删除一些多余文件

---
+++ coral-repository/target/generated-sources/java/org/yugh/coral/repository/ddd/QAbstractEntity.java
@@ -0,0 +1,43 @@
+package org.yugh.coral.repository.ddd;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAbstractEntity is a Querydsl query type for AbstractEntity
+ */
+@Generated("com.querydsl.codegen.SupertypeSerializer")
+public class QAbstractEntity extends EntityPathBase<AbstractEntity> {
+
+    private static final long serialVersionUID = -2027520625L;
+
+    public static final QAbstractEntity abstractEntity = new QAbstractEntity("abstractEntity");
+
+    public final DateTimePath<java.time.Instant> createdAt = createDateTime("createdAt", java.time.Instant.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final DateTimePath<java.time.Instant> updatedAt = createDateTime("updatedAt", java.time.Instant.class);
+
+    public final NumberPath<Integer> version = createNumber("version", Integer.class);
+
+    public QAbstractEntity(String variable) {
+        super(AbstractEntity.class, forVariable(variable));
+    }
+
+    public QAbstractEntity(Path<? extends AbstractEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAbstractEntity(PathMetadata metadata) {
+        super(AbstractEntity.class, metadata);
+    }
+
+}
+

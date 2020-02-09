e69622d8a0e6c5cf26b4fd5521bb019a29e9289a
Genhai Yu <genhaiyu@genhai.io>
2020-02-09 23:11:42 +0800

    Fix 组件生成分布式ID方式

--- coral-core/src/main/java/org/yugh/coral/core/config/distribute/SimpleSnowFlakeGenerated.java
+++ coral-core/src/main/java/org/yugh/coral/core/config/distribute/SimpleSnowFlakeGenerated.java
@@ -1,29 +1,25 @@
-package org.yugh.coral.core.utils;
+package org.yugh.coral.core.config.distribute;
 
 
 import org.springframework.util.Assert;
-import org.yugh.coral.core.config.distribute.SimpleDistributeConfig;
 import org.yugh.coral.core.config.handler.ApplicationContextHelper;
 
 /**
  * @author yugenhai
  */
-public class SnowFlakeGenerateUtils {
+public class SimpleSnowFlakeGenerated {
 
     private static final String SIMPLE_DISTRIBUTE_CONFIG_NAME = "simpleDistributeConfig";
 
-
     /**
-     * see ${@link SimpleDistributeConfig}
-     *
      * @return Long
      */
     public static Long snowFlakeGenerate() {
         SimpleDistributeConfig simpleDistributeConfig = ApplicationContextHelper.getBean(SIMPLE_DISTRIBUTE_CONFIG_NAME);
         Assert.notNull(simpleDistributeConfig, () -> "simpleDistributeConfig '" + simpleDistributeConfig + "' failed to load");
-        if (simpleDistributeConfig.getWorkId() > 0 && simpleDistributeConfig.getDataCenterId() > 0) {
-            return new SnowFlakeGenerated(simpleDistributeConfig.getWorkId(), simpleDistributeConfig.getDataCenterId()).nextId();
+        if (simpleDistributeConfig.getDatacenterId() > 0x00 && simpleDistributeConfig.getMachineId() > 0x00) {
+            return new SnowFlakeGenerated(simpleDistributeConfig.getDatacenterId(), simpleDistributeConfig.getMachineId()).nextId();
         }
-        return new SnowFlakeGenerated(0, 0).nextId();
+        return new SnowFlakeGenerated(0x00, 0x00).nextId();
     }
 }

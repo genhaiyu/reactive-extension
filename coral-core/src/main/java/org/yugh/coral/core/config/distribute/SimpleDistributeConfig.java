64614eb70303b5ff550b36da03ff0da59975ce68
Genhai Yu <genhaiyu@gmail.com>
2020-02-09 23:11:42 +0800

    Fix 组件生成分布式ID方式

--- coral-core/src/main/java/org/yugh/coral/core/config/distribute/SimpleDistributeConfig.java
+++ coral-core/src/main/java/org/yugh/coral/core/config/distribute/SimpleDistributeConfig.java
@@ -12,12 +12,12 @@ public class SimpleDistributeConfig {
     /**
      * 工作 ID (0~31)
      */
-    @Value("${distribute.sequences.work.work-id:0}")
-    private int workId;
+    @Value("${distribute.sequences.datacenter.datacenter-id:0}")
+    private int datacenterId;
 
     /**
      * 数据中心 ID (0~31)
      */
-    @Value("${distribute.sequences.datacenter.datacenter-id:0}")
-    private int dataCenterId;
+    @Value("${distribute.sequences.machine.machine-id:0}")
+    private int machineId;
 }

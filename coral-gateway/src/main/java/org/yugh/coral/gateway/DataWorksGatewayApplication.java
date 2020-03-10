3218670676abb7ed14764cfca0be3265289b3dc0
Genhai <genhai@genhai.dev>
2020-03-10 13:58:02 +0800

    Fix Gateway Use Jetty

--- coral-gateway/src/main/java/org/yugh/coral/gateway/DataWorksGatewayApplication.java
+++ coral-gateway/src/main/java/org/yugh/coral/gateway/DataWorksGatewayApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.reactive.config.EnableWebFlux;
 import org.yugh.coral.gateway.filter.GatewayDataWorksFilter;
 import org.yugh.coral.gateway.reactor.GatewayKeyResolver;
 
-@EnableWebFlux
 @EnableFeignClients
 @ComponentScan(basePackages = {"org.yugh.coral"})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})

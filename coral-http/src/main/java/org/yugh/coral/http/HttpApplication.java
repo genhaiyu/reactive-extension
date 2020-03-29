ba83b57f6db5e6612463790bbbea7df53731bac3
Genhai Yu <genhaiyu@gmail.com>
2020-03-30 00:29:49 +0800

    Fix Null exception

--- coral-http/src/main/java/org/yugh/coral/http/HttpApplication.java
+++ coral-http/src/main/java/org/yugh/coral/http/HttpApplication.java
@@ -3,10 +3,8 @@ package org.yugh.coral.http;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableCaching
-@EnableDiscoveryClient
 @SpringCloudApplication
 public class HttpApplication {
 

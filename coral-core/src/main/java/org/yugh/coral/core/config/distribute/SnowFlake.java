c4316f97ba04b922744eee414fa7db4868528099
Genhai <genhai@genhai.dev>
2020-04-01 14:37:37 +0800

    update concurrent field, update copyright

--- coral-core/src/main/java/org/yugh/coral/core/config/distribute/SnowFlake.java
+++ coral-core/src/main/java/org/yugh/coral/core/config/distribute/SnowFlake.java
@@ -11,7 +11,9 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
+ *
  */
+
 
 package org.yugh.coral.core.config.distribute;
 

a86167161a5a208916ba5ddabde472ffad61d937
Genhai <genhai@genhai.dev>
2020-04-21 10:58:06 +0800

    add rsocket server-client

---
+++ coral-client/src/main/java/org/yugh/coral/client/MainClientApplication.java
@@ -0,0 +1,15 @@
+package org.yugh.coral.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yugenhai
+ */
+@SpringBootApplication
+public class MainClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainClientApplication.class);
+    }
+}

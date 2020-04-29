ca74f80ec4dfc607534a13452e5603baf050119c
Genhai <genhai@genhai.dev>
2020-04-30 01:25:44 +0800

    first run MusicServerApplication, next run MusicClientApplication

--- coral-client/src/main/java/org/yugh/coral/client/rsocket/client/MusicClientController.java
+++ coral-client/src/main/java/org/yugh/coral/client/rsocket/client/MusicClientController.java
@@ -5,13 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.yugh.coral.client.rsocket.model.MusicData;
 import org.yugh.coral.client.rsocket.model.MusicDataRequest;
 
 /**
  * @author yugenhai
  */
-// @RestController
+@RestController
 public class MusicClientController {
 
     private final RSocketRequester rSocketRequester;

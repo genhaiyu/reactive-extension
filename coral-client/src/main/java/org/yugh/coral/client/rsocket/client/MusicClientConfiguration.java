0c3c8353c6d89a6f0480ed141cd04b2b7cf0af12
Genhai Yu <genhaiyu@gmail.com>
2020-04-30 01:25:44 +0800

    first run MusicServerApplication, next run MusicClientApplication

--- coral-client/src/main/java/org/yugh/coral/client/rsocket/client/MusicClientConfiguration.java
+++ coral-client/src/main/java/org/yugh/coral/client/rsocket/client/MusicClientConfiguration.java
@@ -5,11 +5,12 @@ import io.rsocket.RSocketFactory;
 import io.rsocket.frame.decoder.PayloadDecoder;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.util.MimeTypeUtils;
 
-// @Configuration
+@Configuration
 public class MusicClientConfiguration {
 
     @Bean
@@ -17,7 +18,7 @@ public class MusicClientConfiguration {
         return RSocketFactory.connect()
                              .mimeType(MimeTypeUtils.APPLICATION_JSON_VALUE, MimeTypeUtils.APPLICATION_JSON_VALUE)
                              .frameDecoder(PayloadDecoder.ZERO_COPY)
-                             .transport(TcpClientTransport.create(7000))
+                             .transport(TcpClientTransport.create(8083))
                              .start()
                              .block();
     }

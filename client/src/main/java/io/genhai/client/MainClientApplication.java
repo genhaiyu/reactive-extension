67459ef6d72610aad9b97a2980fbbbf3e22444a7
Genhai Yu <genhaiyu@gmail.com>
2020-05-01 23:04:03 +0800

    Learning RSocket

--- client/src/main/java/io/genhai/client/MainClientApplication.java
+++ client/src/main/java/io/genhai/client/MainClientApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * First run {@link io.genhai.client.rsocket.server.MusicServerApplication}
+ * Next run {@link io.genhai.client.rsocket.client.MusicClientApplication}
+ *
  * @author yugenhai
  */
 @SpringBootApplication
